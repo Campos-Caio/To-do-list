93184bbacc09242961b09ce7452e722c2a8bd6b5
Caio Campos <caioalvescampos2010@gmail.com>
2024-05-28 16:39:01 -0400

    Commit final

--- to-do-list-/script.js
+++ to-do-list-/script.js
@@ -1,4 +1,4 @@
-const localStorageKey = 'to-do-list-gn';
+const localStorageKey = 'to-do-list';
 
 function validateIfExistsNewTask() {
     let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
@@ -9,6 +9,7 @@ function validateIfExistsNewTask() {
 
 function newTask() {
     let input = document.getElementById('input-new-task');
+    let colorInput = document.getElementById('input-task-color');
     input.style.border = '';
 
     // validation
@@ -20,11 +21,12 @@ function newTask() {
     } else {
         // increment to localStorage
         let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
-        values.push({ name: input.value, completed: false });
+        values.push({ name: input.value, completed: false, color: colorInput.value });
         localStorage.setItem(localStorageKey, JSON.stringify(values));
         showValues();
     }
     input.value = '';
+    colorInput.value = '#000000';
 }
 
 function showValues() {
@@ -33,25 +35,26 @@ function showValues() {
     list.innerHTML = '';
     for (let i = 0; i < values.length; i++) {
         let completedClass = values[i].completed ? 'completed' : '';
+        let backgroundColor = values[i].completed ? 'var(--color-green)' : values[i].color;
         list.innerHTML += `
-          <li class="${completedClass}">
+          <li class="${completedClass}" style="background-color: ${backgroundColor}">
             <span class="task-name">${values[i]['name']}</span>
             <div class="button-container">
-              <button class="btn-edit" onclick='editItem("${values[i]['name']}")' title="Editar">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-                  <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zm1.415 3L10.5 1.707 14.293 5.5 13.56 6.233 10.293 3.707 2 12V13h1L12 4.707 13.56 3.146zM1 13.5V15h1.5L13.293 5.707l-1.5-1.5L1 13.5z"/>
+                <button class="btn-edit" onclick='editItem("${values[i]['name']}")' title="Editar">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zm1.415 3L10.5 1.707 14.293 5.5 13.56 6.233 10.293 3.707 2 12V13h1L12 4.707 13.56 3.146zM1 13.5V15h1.5L13.293 5.707l-1.5-1.5L1 13.5z"/>
+                    </svg>
+                </button>
+                <button class="btn-ok" onclick='completeItem("${values[i]['name']}")' title="Concluir">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                    </svg>
+                </button>
+                <button class="btn-remove" onclick='removeItem("${values[i]['name']}")' title="Remover">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-trash-simple" viewBox="0 0 16 16">
+                    <path d="M2 4a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v1H2V4zm11 1V15a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5h10z"/>
                 </svg>
-              </button>
-              <button class="btn-ok" onclick='completeItem("${values[i]['name']}")' title="Concluir">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
-                  <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
-                </svg>
-              </button>
-              <button class="btn-remove" onclick='removeItem("${values[i]['name']}")' title="Remover">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-trash" viewBox="0 0 24 24">
-                  <path d="M3 6h18v2H3V6zm2.5 3h13l-1 12H6.5l-1-12zm4 12h5l.5-9h-6l.5 9zM9 4V2.5C9 2.224 9.224 2 9.5 2h5c.276 0 .5.224.5.5V4h6.5a1 1 0 0 1 0 2h-18a1 1 0 0 1 0-2H9z"/>
-                </svg>
-              </button>
+                </button>
             </div>
           </li>`;
     }
@@ -92,5 +95,3 @@ function removeItem(name) {
     localStorage.setItem(localStorageKey, JSON.stringify(values));
     showValues();
 }
-
-showValues();
